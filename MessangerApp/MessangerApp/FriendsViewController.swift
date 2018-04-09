26fb0c88e756634aceb9ea6ba01aa0ce6b287fc5
Paweł Liczmański <paweliczek@gmail.com>
2018-04-09 19:53:10 +0200

    create collection view with cell

--- MessangerApp/MessangerApp/FriendsViewController.swift
+++ MessangerApp/MessangerApp/FriendsViewController.swift
@@ -10,12 +10,52 @@ import UIKit
 
 class FriendsViewController: UICollectionViewController {
 
+	private let cellID = "cellID"
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		collectionView?.backgroundColor = UIColor.green
+		
+		collectionView?.register(FriendCell.self , forCellWithReuseIdentifier: cellID)
+	}
+	
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 3
+	}
+	
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		return collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
 	}
 
 
 }
+
+class FriendCell: UICollectionViewCell {
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupCell()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func setupCell() {
+		self.backgroundColor = UIColor.red
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
