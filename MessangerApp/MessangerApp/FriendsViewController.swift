a86c031a1188a9f903bdb1384f4bf33d2676ac85
Paweł Liczmański <paweliczek@gmail.com>
2018-04-09 19:39:46 +0200

    window  zamiast storyborda

---
+++ MessangerApp/MessangerApp/FriendsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MessangerApp
+//
+//  Created by Paweł Liczmański on 09.04.2018.
+//  Copyright © 2018 Paweł Liczmański. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewController: UICollectionViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		collectionView?.backgroundColor = UIColor.green
+	}
+
+
+}
+
