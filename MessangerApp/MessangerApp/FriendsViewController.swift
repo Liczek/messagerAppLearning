b7abe8a0cd501e86f81add749bb59b6046a2c47c
Paweł Liczmański <paweliczek@gmail.com>
2018-04-09 21:50:35 +0200

    add collectionview delegate flow layout / add profileimageview / set constraints/ set always bouce collection view/ set size for item

--- MessangerApp/MessangerApp/FriendsViewController.swift
+++ MessangerApp/MessangerApp/FriendsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FriendsViewController: UICollectionViewController {
+class FriendsViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
 	private let cellID = "cellID"
 	
@@ -16,6 +16,7 @@ class FriendsViewController: UICollectionViewController {
 		super.viewDidLoad()
 		
 		collectionView?.backgroundColor = UIColor.green
+		collectionView?.alwaysBounceVertical = true
 		
 		collectionView?.register(FriendCell.self , forCellWithReuseIdentifier: cellID)
 	}
@@ -28,10 +29,37 @@ class FriendsViewController: UICollectionViewController {
 		return collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
 	}
 
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		return CGSize(width: view.frame.size.width, height: 100)
+	}
+	
 
 }
 
-class FriendCell: UICollectionViewCell {
+class FriendCell: BaseCell {
+	
+	let profileImageView: UIImageView = {
+		let imageView = UIImageView()
+		imageView.image = UIImage(named: "zuckprofile")
+		imageView.contentMode = .scaleAspectFit
+		return imageView
+	}()
+	
+	override func setupCell() {
+		self.backgroundColor = UIColor.red
+		
+		self.addSubview(profileImageView)
+		profileImageView.translatesAutoresizingMaskIntoConstraints = false
+		profileImageView.layer.masksToBounds = true
+		profileImageView.layer.cornerRadius = 34
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-12-[v0(68)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": profileImageView]))
+		
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-12-[v0(68)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": profileImageView]))
+
+	}
+}
+
+class BaseCell: UICollectionViewCell {
 	
 	override init(frame: CGRect) {
 		super.init(frame: frame)
@@ -46,7 +74,6 @@ class FriendCell: UICollectionViewCell {
 		self.backgroundColor = UIColor.red
 	}
 }
-
 
 
 
