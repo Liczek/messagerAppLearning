908b3571f2611bac12fcae6c88a70184d73b7ee2
Paweł Liczmański <paweliczek@gmail.com>
2018-04-10 20:56:04 +0200

    create containerView for name label message label time label hasReadImageView  using centerY constraints for container (addConstraint(NSConstraint(item....

--- MessangerApp/MessangerApp/FriendsViewController.swift
+++ MessangerApp/MessangerApp/FriendsViewController.swift
@@ -51,22 +51,80 @@ class FriendCell: BaseCell {
 		return lineView
 	}()
 	
+	let containerView: UIView = {
+		let containerView = UIView()
+		containerView.backgroundColor = UIColor.white
+		return containerView
+	}()
+	
+	let nameLabel: UILabel = {
+		let label = UILabel()
+		label.text = "Mark Zuckenberg"
+		label.font = UIFont.boldSystemFont(ofSize: 18)
+		return label
+	}()
+	
+	let messageLabel: UILabel = {
+		let label = UILabel()
+		label.text = "Jakaś treść wiadomości na tyle długa, żeby nie zmieściła się w jednej linijce"
+		label.font = UIFont.systemFont(ofSize: 14)
+		return label
+	}()
+	
+	let timeLabel: UILabel = {
+	let label = UILabel()
+		label.text = "12:06 pm"
+		label.font = UIFont.systemFont(ofSize: 14)
+		label.textAlignment = .right
+	return label
+	}()
+	
+	let hasReadImageView: UIImageView = {
+		let imageView = UIImageView()
+		imageView.image = UIImage(named: "zuckprofile")
+		imageView.contentMode = .scaleAspectFit
+		imageView.layer.masksToBounds = true
+		imageView.layer.cornerRadius = 10
+		return imageView
+	}()
+	
+	private func setupContainerView() {
+		containerView.addSubview(nameLabel)
+		containerView.addSubview(messageLabel)
+		containerView.addSubview(timeLabel)
+		containerView.addSubview(hasReadImageView)
+		
+		addConstraintsWithVisualFormat(format: "H:|[v0][v1(80)]-8-|", views: nameLabel, timeLabel)
+		addConstraintsWithVisualFormat(format: "V:|[v0][v1(24)]|", views: nameLabel, messageLabel)
+		addConstraintsWithVisualFormat(format: "H:|[v0]-8-[v1(20)]-8-|", views: messageLabel, hasReadImageView)
+//		addConstraintsWithVisualFormat(format: "V:|[v0(24)]", views: timeLabel)
+		addConstraint(NSLayoutConstraint(item: timeLabel, attribute: .centerY, relatedBy: .equal, toItem: nameLabel, attribute: .centerY, multiplier: 1, constant: 0))
+		addConstraintsWithVisualFormat(format: "V:[v0(20)]|", views: hasReadImageView)
+	}
+	
 	override func setupCell() {
 		
 		self.addSubview(profileImageView)
 		self.addSubview(dividerLineView)
-		profileImageView.translatesAutoresizingMaskIntoConstraints = false
-		dividerLineView.translatesAutoresizingMaskIntoConstraints = false
+		self.addSubview(containerView)
+		setupContainerView()
 		profileImageView.layer.masksToBounds = true
 		profileImageView.layer.cornerRadius = 34
 
-		addConstraintsWithVisualFormat(format: "H:|-12-[v0(68)]", views: profileImageView)
-		addConstraintsWithVisualFormat(format: "V:|-12-[v0(68)]", views: profileImageView)
+		addConstraintsWithVisualFormat(format: "H:|-12-[v0(68)]-8-[v1]|", views: profileImageView, containerView)
+		addConstraintsWithVisualFormat(format: "V:[v0(68)]", views: profileImageView)
+		addConstraint(NSLayoutConstraint(item: profileImageView	, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+
 		
 		addConstraintsWithVisualFormat(format: "H:|-86-[v0]|", views: dividerLineView)
 		addConstraintsWithVisualFormat(format: "V:[v0(1)]|", views: dividerLineView)
+		
+		addConstraintsWithVisualFormat(format: "V:[v0(68)]", views: containerView)
+		addConstraint(NSLayoutConstraint(item: containerView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
 
 	}
+	
+	
 }
 
 extension UIView {
